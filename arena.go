1e91f5c4e75a6f1f2f3fb9417b9623313376d1b1
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-01-04 11:17:35 +0300

    initial implementation

---
+++ arena.go
@@ -0,0 +1,113 @@
+package malloc
+
+import (
+	"sync"
+	"unsafe"
+)
+
+type Arena struct {
+	mu       sync.Mutex
+	buff     []byte
+	freeList map[int]int
+}
+
+func NewArena(cap int) *Arena {
+	a := &Arena{
+		buff:     make([]byte, cap),
+		freeList: make(map[int]int),
+	}
+	a.freeList[0] = cap
+	return a
+}
+
+func (a *Arena) Alloc(size int) []byte {
+	if size <= 0 {
+		return nil
+	}
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	offset, length := -1, int(^uint(0)>>1)
+	for k, v := range a.freeList {
+		if v >= size && v < length && k > offset {
+			offset = k
+			length = v
+		}
+	}
+	if offset < 0 {
+		return nil
+	}
+	foundOffset := offset
+	ending := offset + length
+	lengthHigh := highbit(length - 1)
+	sizeHigh := highbit(size - 1)
+	for offset < ending {
+		if lengthHigh > sizeHigh {
+			lengthHigh--
+			length = 1 << uint(lengthHigh)
+		}
+		l := ending - offset
+		if l < length {
+			length = l
+		}
+		a.freeList[offset] = length
+		//fmt.Println("write:", offset, length)
+		if length >= size {
+			foundOffset = offset
+		}
+		offset += length
+	}
+	offset = foundOffset
+	length = a.freeList[offset]
+	delete(a.freeList, offset)
+	//fmt.Println("found:", offset, length)
+	return a.buff[offset : offset+size]
+}
+
+func (a *Arena) Free(ptr []byte) {
+	a.mu.Lock()
+	defer a.mu.Unlock()
+	ptrSize := len(ptr)
+	if ptrSize <= 0 {
+		return
+	}
+	ptrOffset := int(uintptr(unsafe.Pointer(&ptr[0])) - uintptr(unsafe.Pointer(&a.buff[0])))
+	if ptrOffset >= len(a.buff) {
+		return
+	}
+	ptrHigh := highbit(ptrSize - 1)
+	ptrLength := 1 << uint(ptrHigh)
+	offset := ptrOffset
+	length := ptrLength
+	firstOffset := offset
+	for {
+		freed := false
+		offset = firstOffset + length
+		if l, ok := a.freeList[offset]; ok {
+			if l == length {
+				delete(a.freeList, offset)
+				length *= 2
+				a.freeList[firstOffset] = length
+				//fmt.Println("fwrite:", firstOffset, length)
+				freed = true
+			}
+		}
+		offset = firstOffset - length
+		if l, ok := a.freeList[offset]; ok {
+			if l == length {
+				delete(a.freeList, firstOffset)
+				length *= 2
+				a.freeList[offset] = length
+				//fmt.Println("rwrite:", offset, length)
+				freed = true
+				firstOffset = offset
+			}
+		}
+		if !freed {
+			if firstOffset == ptrOffset {
+				a.freeList[firstOffset] = length
+				//fmt.Println("lwrite:", firstOffset, length)
+			}
+			break
+		}
+	}
+}
