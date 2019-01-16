87d3b122d2ab65c186af87c45064b4bd1793c5f2
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-01-16 22:04:58 +0300

    zelus-server first testing

--- cmd/zelus-server/main.go
+++ cmd/zelus-server/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	_ "net/http/pprof"
+
 	"github.com/orkunkaraduman/zelus/pkg/server"
 	"github.com/orkunkaraduman/zelus/pkg/store"
 )
 
 func main() {
-	st := store.New(1000, 1*1024*1024)
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	size := 10 * 1024 * 1024 * 1024
+	count := 2 * size / 4096
+	st := store.New(count, size)
 	srv := server.New(st)
-	srv.ListenAndServe("0.0.0.0:1234")
+	//srv.TCPListenAndServe("0.0.0.0:1234")
+	//lst, err := net.Listen("unix", "/tmp/zelus.sock")
+	lst, err := net.Listen("tcp", "127.0.0.1:1234")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("ready")
+	srv.Serve(lst)
 }
