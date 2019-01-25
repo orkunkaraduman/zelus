643f78359e9f1602af90876e4a4c14cd5a83f9a9
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-01-25 13:51:46 +0300

    pkg server; implemented REPLACE, APPEND, DEL commands

--- pkg/server/connstate.go
+++ pkg/server/connstate.go
@@ -2,6 +2,8 @@ package server
 
 import (
 	"net"
+	"strconv"
+	"time"
 
 	"github.com/orkunkaraduman/zelus/pkg/buffer"
 	"github.com/orkunkaraduman/zelus/pkg/protocol"
@@ -34,6 +36,13 @@ func (cs *connState) OnReadCmd(cmd protocol.Cmd) (count int) {
 		count = -1
 		return
 	}
+	if cs.rCmd.Name == "PING" {
+		err = cs.SendCmd(protocol.Cmd{Name: "PONG", Args: []string{strconv.Itoa(int(time.Now().UnixNano() / 1000))}})
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
 	if cs.rCmd.Name == "GET" {
 		keys := make([]string, 0, len(cs.rCmd.Args))
 		for _, key := range cs.rCmd.Args {
@@ -64,7 +73,7 @@ func (cs *connState) OnReadCmd(cmd protocol.Cmd) (count int) {
 		}
 		return
 	}
-	if cs.rCmd.Name == "SET" {
+	if cs.rCmd.Name == "SET" || cs.rCmd.Name == "REPLACE" || cs.rCmd.Name == "APPEND" {
 		count = len(cs.rCmd.Args)
 		if count > 0 {
 			return
@@ -75,16 +84,44 @@ func (cs *connState) OnReadCmd(cmd protocol.Cmd) (count int) {
 		}
 		return
 	}
+	if cs.rCmd.Name == "DEL" {
+		keys := make([]string, 0, len(cs.rCmd.Args))
+		for _, key := range cs.rCmd.Args {
+			if key == "" {
+				continue
+			}
+			if !cs.st.Del(key) {
+				continue
+			}
+			keys = append(keys, key)
+		}
+		err = cs.SendCmd(protocol.Cmd{Name: "OK", Args: keys})
+		if err != nil {
+			panic(err)
+		}
+		return
+	}
 	panic(&protocol.Error{Err: ErrUnknownCommand, Cmd: cs.rCmd})
 }
 
 func (cs *connState) OnReadData(count int, index int, data []byte) {
 	var err error
-	if cs.rCmd.Name == "SET" {
+	if cs.rCmd.Name == "SET" || cs.rCmd.Name == "REPLACE" || cs.rCmd.Name == "APPEND" {
 		key := cs.rCmd.Args[index]
 		if key != "" {
-			if !cs.st.Set(key, data, true) {
-				cs.rCmd.Args[index] = ""
+			switch cs.rCmd.Name {
+			case "SET":
+				if !cs.st.Set(key, data) {
+					cs.rCmd.Args[index] = ""
+				}
+			case "REPLACE":
+				if !cs.st.Replace(key, data) {
+					cs.rCmd.Args[index] = ""
+				}
+			case "APPEND":
+				if !cs.st.Append(key, data) {
+					cs.rCmd.Args[index] = ""
+				}
 			}
 		}
 		if index+1 >= count {
