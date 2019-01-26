3b406f145dcf06a69d61dd161ac9980c716aaab4
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-01-27 01:17:41 +0300

    zelus-server is pre-prod state now

--- cmd/zelus-server/main.go
+++ cmd/zelus-server/main.go
@@ -1,34 +1,91 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"net/http"
 	_ "net/http/pprof"
 	"os"
+	"os/signal"
+	"time"
 
+	"github.com/orkunkaraduman/zelus/pkg/malloc"
+	"github.com/orkunkaraduman/zelus/pkg/protocol"
 	"github.com/orkunkaraduman/zelus/pkg/server"
 	"github.com/orkunkaraduman/zelus/pkg/store"
 )
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
+	fmt.Printf("zelus-server\n\n")
+	pp := os.Getenv("PPROF")
+	if pp != "" {
+		go func() {
+			log.Fatalln(http.ListenAndServe(pp, nil))
+		}()
+	}
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+	logger.Println("Started zelus-server")
+	var err error
+	ca := newCmdArgs(os.Stdout)
+	err = ca.Parse(os.Args[1:])
+	if err != nil {
+		logger.Println("Error:", err.Error())
+		os.Exit(2)
+	}
+	defer func() {
+		if e, ok := recover().(error); ok {
+			fmt.Println("Error:", e.Error())
+			fmt.Println("")
+			logger.Println("Error:", e.Error())
+			os.Exit(1)
+		}
+		fmt.Println("")
 	}()
 
-	server.ConnBuffer = 1024 * 1024
-
-	size := 10 * 1024 * 1024 * 1024
-	count := 2 * size / 4096
-	st := store.New(count, size)
-	srv := server.New(st)
-	os.Remove("/tmp/zelus.sock")
-	lst, err := net.Listen("unix", "/tmp/zelus.sock")
-	//lst, err := net.Listen("tcp", "127.0.0.1:1234")
+	logger.Printf("Listening socket...\n")
+	protocol.BufferSize = 1 * 1024 * 1024
+	server.ConnBufferSize = 1 * 1024 * 1024
+	var lst net.Listener
+	var addr string
+	if ca.Socket == "" {
+		lst, err = net.Listen("tcp", ca.Bind)
+		addr = ca.Bind
+	} else {
+		os.Remove(ca.Socket)
+		lst, err = net.Listen("unix", ca.Socket)
+		addr = ca.Socket
+	}
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("ready")
-	srv.Serve(lst)
+
+	logger.Printf("Allocating %dMiB of memory...\n", ca.Capacity)
+	size := int(ca.Capacity) * 1024 * 1024
+	count := size / (4 * malloc.MinLength)
+	st := store.New(count, size)
+	srv := server.New(st)
+
+	logger.Printf("Accepting connections from %s", addr)
+	sigIntCh := make(chan os.Signal, 1)
+	signal.Notify(sigIntCh, os.Interrupt)
+	go func() {
+		time.Sleep(250 * time.Microsecond)
+		<-sigIntCh
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		srv.Shutdown(ctx)
+		cancel()
+	}()
+	sigKillCh := make(chan os.Signal, 1)
+	signal.Notify(sigKillCh, os.Kill)
+	go func() {
+		time.Sleep(250 * time.Microsecond)
+		<-sigKillCh
+		srv.Close()
+	}()
+	err = srv.Serve(lst)
+	if err != nil {
+		panic(err)
+	}
 }
