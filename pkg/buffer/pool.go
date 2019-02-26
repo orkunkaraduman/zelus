83da432d2cbba88e69bf7eb67d7200d42eb8b56b
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-02-26 15:28:10 +0300

    pkg buffer; added Pool

---
+++ pkg/buffer/pool.go
@@ -0,0 +1,62 @@
+package buffer
+
+import "sync"
+
+type Pool struct {
+	mu sync.RWMutex
+	qu chan *Buffer
+}
+
+func NewPool(max int) (p *Pool) {
+	if max <= 0 {
+		return
+	}
+	p = &Pool{
+		qu: make(chan *Buffer, max),
+	}
+	return
+}
+
+func (p *Pool) Close() {
+	p.mu.Lock()
+	if p.qu == nil {
+		p.mu.Unlock()
+		return
+	}
+	for len(p.qu) > 0 {
+		bf := <-p.qu
+		bf.Close()
+	}
+	close(p.qu)
+	p.qu = nil
+	p.mu.Unlock()
+}
+
+func (p *Pool) Get() (bf *Buffer) {
+	p.mu.RLock()
+	if p.qu == nil {
+		p.mu.RUnlock()
+		return
+	}
+	select {
+	case bf = <-p.qu:
+	default:
+	}
+	p.mu.RUnlock()
+	return
+}
+
+func (p *Pool) Put(bf *Buffer) (suc bool) {
+	p.mu.RLock()
+	if p.qu == nil {
+		p.mu.RUnlock()
+		return
+	}
+	select {
+	case p.qu <- bf:
+		suc = true
+	default:
+	}
+	p.mu.RUnlock()
+	return
+}
