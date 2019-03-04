0fff6c254b3d5057d55257c51f0220cd2f637fc7
Orkun Karaduman <orkunkaraduman@gmail.com>
2019-03-05 00:03:47 +0300

    pkg utils; bug fix to prevent garbage collector moving object(string data).

--- pkg/utils/reflect.go
+++ pkg/utils/reflect.go
@@ -17,8 +17,27 @@ func ChangeSliceType(src interface{}, count int, typ reflect.Type) interface{} {
 	return dstVal.Slice(0, count).Interface()
 }
 
-func StringToByteSlice(s string) []byte {
-	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+func StringToByteSlice(src string) (dst []byte) {
+	// correct implementation with native.
+	// 'p := unsafe.Pointer(&src)' is key point.
+	// Also dh.Cap assignment is must.
+	// (*reflect.StringHeader)(p) can be used instead of sh.
+	p := unsafe.Pointer(&src)
+	sh := (*reflect.StringHeader)(p)
+	dh := (*reflect.SliceHeader)(unsafe.Pointer(&dst))
+	dh.Data = uintptr(unsafe.Pointer(sh.Data))
+	dh.Len = sh.Len
+	dh.Cap = sh.Len
+	return
+
+	// correct implementation with reflect
+	/*p := unsafe.Pointer(&src)
+	sh := (*reflect.StringHeader)(p)
 	bVal := reflect.NewAt(reflect.ArrayOf(sh.Len, typeOfByte), unsafe.Pointer(sh.Data)).Elem()
-	return bVal.Slice(0, sh.Len).Interface().([]byte)
+	return bVal.Slice(0, sh.Len).Interface().([]byte)*/
+
+	// wrong implementation with reflect
+	/*sh := (*reflect.StringHeader)(unsafe.Pointer(&src))
+	bVal := reflect.NewAt(reflect.ArrayOf(sh.Len, typeOfByte), unsafe.Pointer(sh.Data)).Elem()
+	return bVal.Slice(0, sh.Len).Interface().([]byte)*/
 }
